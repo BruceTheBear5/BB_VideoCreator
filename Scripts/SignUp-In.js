054f3848e0c294873ebc84e0e0cf5612d9418d5f
Saiyam20 <saiyam.jain@students.iiit.ac.in>
2024-02-07 17:46:54 +0530

    SignUp-Admin

--- Scripts/SignUp-In.js
+++ Scripts/SignUp-In.js
@@ -1,5 +1,8 @@
 let signin = document.getElementById("signin");
 let signup = document.getElementById("signup");
+let admin = document.getElementById("Admin");
+let normal = document.getElementById("Normal");
+let error = document.getElementById("Error");
 
 function toggleForms() {
     if (signin.style.display === "none") {
@@ -8,5 +11,35 @@ function toggleForms() {
     } else {
         signin.style.display = "none";
         signup.style.display = "flex";
+        admin.style.display = "none";
+        normal.style.display = "none";
+        error.style.display = "none";
     }
+}
+
+function signIn() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username == "admin" && password == "admin") {
+        console.log("1");
+        admin.style.display = "flex";
+        normal.style.display = "none";
+        error.style.display = "none";
+    }
+    else if (username == "user" && password == "1234") {
+        normal.style.display = "flex";
+        admin.style.display = "none";
+        error.style.display = "none";
+    }
+    else {
+        error.style.display = "flex";
+        admin.style.display = "none";
+        normal.style.display = "none";
+        return;
+    }
+
+    setTimeout(function () {
+        window.location.href = "index.html";
+    }, 3000);
 }
