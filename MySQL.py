2cc67266aff2e86cf3cf080a44a4ebe7026aa519
Saiyam20 <saiyam.jain@students.iiit.ac.in>
2024-02-19 22:30:16 +0530

    Retrieve

--- MySQL.py
+++ MySQL.py
@@ -1,12 +1,24 @@
 import mysql.connector
 
 class User:
-    def __init__(self,name, username, email, password, isAdmin=False):
+    def __init__(self, name, username, email, password, id=-1, isAdmin=False):
+        self.id = id
         self.name = name
         self.username = username
         self.email = email
         self.password = password
         self.isAdmin = isAdmin
+        
+    def printUser(self):
+        if (self != None):
+            print("User ID:", self.id)
+            print("Name:", self.name)
+            print("Username:", self.username)
+            print("Email:", self.email)
+            print("Password:", self.password)
+            print("Is Admin:", self.isAdmin)
+        else:
+            print("User not found.")
         
 class Image:
     def __init__(self, file_name, image_len, file_type, file_data):
@@ -15,7 +27,7 @@ class Image:
         self.file_type = file_type
         self.file_data = file_data
 
-def save_to_mysql(data):
+def save_data_mysql(data):
     try:
         connection = mysql.connector.connect(
             host="localhost",
@@ -45,22 +57,73 @@ def save_to_mysql(data):
             cursor.close()
             connection.close()
             
-def save_image(user_id, imageName):
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Saiyam20_",
-        database="Project"
-    )
-    
-    cursor = conn.cursor()
-    with open(imageName, "rb") as file:
-        image_data = file.read()
+def save_image_to_mysql(user_id, image_name):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Saiyam20_",
+            database="Project"
+        )
 
-    insert_query = """INSERT INTO images (user_id, file_name, file_size, file_type, file_data) VALUES (%s, %s, %s, %s, %s)"""
-    image_values = (user_id, "image.jpg", len(image_data), "image/jpeg", image_data)
-    cursor.execute(insert_query, image_values)
+        cursor = connection.cursor()
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+        with open(image_name, "rb") as file:
+            image_data = file.read()
+
+        insert_query = """INSERT INTO images (user_id, file_name, file_size, file_type, file_data) VALUES (%s, %s, %s, %s, %s)"""
+        image_values = (user_id, image_name, len(image_data), "image/jpeg", image_data)
+        cursor.execute(insert_query, image_values)
+
+        connection.commit()
+        print("Image saved to MySQL database successfully.")
+
+    except mysql.connector.Error as error:
+        print("Failed to save image to MySQL database:", error)
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            
+def retrieve_users_from_mysql(email):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Saiyam20_",
+            database="Project"
+        )
+
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM Users WHERE Users.email = %s"
+        cursor.execute(query, (email, ))
+
+        for (id, name, username, email, password, isAdmin) in cursor:
+            users = User(name, username, email, password, id, isAdmin)
+
+        if(users):
+            print("Data retrieved from MySQL database successfully.")    
+            return users
+        else:
+            print("Failed to retrieve data from MySQL database:")
+            return None
+
+    except mysql.connector.Error as error:
+        print("Failed to retrieve data from MySQL database:", error)
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+users = [
+    User("John Doe", "johndoe", "john@example.com", "password123"),
+    User("Jane Smith", "janesmith", "jane@example.com", "password456", isAdmin=True)
+]
+
+# save_data_mysql(users)
+user = retrieve_users_from_mysql("jane@example.com")
+if(user):
+    user.printUser()
