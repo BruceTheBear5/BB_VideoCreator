395fd466dd8e3ef320db456391159bf6328e95b9
Saiyam20 <saiyam.jain@students.iiit.ac.in>
2024-02-23 21:46:36 +0530

    (.)(.)

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, Response
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 from MySQL import User, retrieve_users_from_mysql, save_data_to_mysql, retrieve_image_from_mysql, retrieve_profile_image, upload_profile_image, AdminRetrieve, AdminRetrieveProfilePic, retrieve_audio_from_mysql, save_image_to_mysql, save_audio_to_mysql
 import os
 import base64
 
 app = Flask(__name__)
-SECRET_KEY =  os.urandom(24)
-app.secret_key = SECRET_KEY
+app.secret_key = os.urandom(24)
+app.config["JWT_TOKEN_LOCATION"] = ["headers","cookies","json","query_string"]
+app.config["JWT_SECRET_KEY"] = os.urandom(24)
+jwt = JWTManager(app)
 
 @app.route('/')
 def home():
-    if "userId" in session:
-        if session["userIsAdmin"] == "True":
-            return render_template('index.html', isAdmin = "True", user = "True", username = session["username"])
+    if 'jwt_token' in session:
+        user_info = session['jwt_token']
+        if user_info.get("isAdmin"):
+            return render_template('index.html', isAdmin="True", user="True", username=user_info.get("username"))
         else:
-            return render_template('index.html', isAdmin = "False", user = "True", username = session["username"])
-    return render_template('index.html', isAdmin = "False", user = "False")
+            return render_template('index.html', isAdmin="False", user="True", username=user_info.get("username"))
+    return render_template('index.html', isAdmin="False", user="False")
 
 @app.route('/signin')
 def signin():
-    return render_template('login.html', signin = "True", signup = "False", ForgetPassword = "False")
+    return render_template('login.html', signin="True", signup="False", ForgetPassword="False")
 
 @app.route('/signup')
 def signup():
-    return render_template('login.html', signin = "False", signup = "True", ForgetPassword = "False")
+    return render_template('login.html', signin="False", signup="True", ForgetPassword="False")
 
 @app.route('/forgetpassword')
 def forgetPassword():
-    return render_template('login.html', signin = "False", signup = "False", ForgetPassword = "True")
+    return render_template('login.html', signin="False", signup="False", ForgetPassword="True")
 
 @app.route('/SignIn', methods=['POST'])
 def signinFunc():
@@ -38,17 +42,16 @@ def signinFunc():
         flash('No User Found')
         return redirect(url_for('signin'))
     elif check_password_hash(user.password, password):
-        session["userId"] = user.id
-        session["username"] = str(user.username)
-        session["userEmail"] = user.email
-        session["userIsAdmin"] = user.isAdmin
+        access_token = create_access_token(identity={"userId": user.id, "username": user.username, "isAdmin": user.isAdmin})
+        user_info = {"userId": user.id, "username": user.username,"email": user.email , "isAdmin": user.isAdmin}
+        session['jwt_token'] = user_info
         if user.isAdmin:
-            flash('Admin SignIn Successfull')
+            flash('Admin SignIn Successful')
         else:
-            flash('SignIn Successfull')
+            flash('SignIn Successful')
         return redirect(url_for('home'))
     else:
-        flash('Incorect password')
+        flash('Incorrect password')
         return redirect(url_for('signin'))
 
 @app.route('/SignUp', methods=['POST'])
@@ -64,185 +67,201 @@ def signupFunc():
         save_data_to_mysql(user)
         user = retrieve_users_from_mysql(email)
         upload_profile_image(user.id, "./static/Images/alt_image.jpg")
-        session["userId"] = user.id
-        session["username"] = user.username
-        session["userEmail"] = user.email
-        session["userIsAdmin"] = user.isAdmin
+        access_token = create_access_token(identity={"userId": user.id, "username": user.username, "isAdmin": user.isAdmin})
+        user_info = {"userId": user.id, "username": user.username,"email": user.email ,"isAdmin": user.isAdmin}
+        session['jwt_token'] = user_info
         flash('Registration Successful')
-        return redirect(url_for('home'))
-        
+        return redirect(url_for('home'))   
     else:
         flash('Email already exists')
         return redirect(url_for('signup'))
-    
+
 @app.route('/profile/<username>', methods=['GET'])
 def profileData(username):
     try:
-        if "userId" in session:
-            UserEmail = session["userEmail"]
-            user = retrieve_users_from_mysql(UserEmail)
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            user = retrieve_users_from_mysql(user_info["email"])
             profile_image = retrieve_profile_image(user.id)
             profileImage = base64.b64encode(profile_image.file_data).decode('utf-8')
             images = retrieve_image_from_mysql(user.id)
             imageData = []
             for i in images:
                 encoded_image = base64.b64encode(i.file_data).decode('utf-8')
-                img = {'data' : encoded_image, 'name': i.file_name}
+                img = {'data': encoded_image, 'name': i.file_name}
                 imageData.append(img)
-                
+
             audio = retrieve_audio_from_mysql(user.id)
             audioData = []
             for a in audio:
                 encoded_audio = base64.b64encode(a.file_data).decode('utf-8')
                 ad = {'data': encoded_audio, 'name': a.file_name}
                 audioData.append(ad)
-                
-            return render_template('profile.html', user = user, profileImage = profileImage, images = imageData, audio = audioData, username = user.username, isAdmin = user.isAdmin)
+
+            return render_template('profile.html', user=user, profileImage=profileImage, images=imageData, audio=audioData, username=user.username, isAdmin=user.isAdmin)
         else:
             return redirect(url_for('home'))
     except Exception as e:
         print("Error:", e)
         return Response(status=500)
-    
+
 @app.route('/Profile/<username>', methods=['POST'])
 def uploadProfileImage(username):
     try:
-        user_id = session["userId"]
-        username = session["username"]
-        image_file = request.files['image']
-        
-        TEMP_DIR = './temp/'
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            user_id = user_info["userId"]
+            image_file = request.files['image']
 
-        if not os.path.exists(TEMP_DIR):
-            os.makedirs(TEMP_DIR)
-        
-        image_path = f"./temp/{user_id}_profile_image.jpg"
-        image_file.save(image_path)
-        
-        upload_profile_image(user_id, image_path)
-                
-        if os.path.exists(TEMP_DIR):
-            for file_name in os.listdir(TEMP_DIR):
-                file_path = os.path.join(TEMP_DIR, file_name)
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
-            os.rmdir(TEMP_DIR)
-        
-        return redirect(url_for('profileData', username=username))
+            TEMP_DIR = './temp/'
+
+            if not os.path.exists(TEMP_DIR):
+                os.makedirs(TEMP_DIR)
+
+            image_path = f"./temp/{user_id}_profile_image.jpg"
+            image_file.save(image_path)
+
+            upload_profile_image(user_id, image_path)
+
+            if os.path.exists(TEMP_DIR):
+                for file_name in os.listdir(TEMP_DIR):
+                    file_path = os.path.join(TEMP_DIR, file_name)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                os.rmdir(TEMP_DIR)
+
+            return redirect(url_for('profileData', username=username))
     except Exception as e:
         print("Error:", e)
         return Response(status=500)
-    
+
 @app.route('/adminPage')
 def Admin():
     try:
-        if "userIsAdmin" in session:
-            if session["userIsAdmin"] == 'True':
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            if user_info["isAdmin"] == 'True':
                 users = AdminRetrieve()
                 profilePictures = AdminRetrieveProfilePic()
-                
+
                 profilePic = []
                 for pic in profilePictures:
                     encoded_image = base64.b64encode(pic.file_data).decode('utf-8')
                     profilePic.append(encoded_image)
 
                 NumberOfAccounts = len(users)
-                return render_template('adminPage.html', NumberOfAccounts = NumberOfAccounts, user = users, profilePic = profilePic, username = session["username"])
+                return render_template('adminPage.html', NumberOfAccounts=NumberOfAccounts, user=users, profilePic=profilePic, username=user_info["username"])
             else:
                 return redirect(url_for('home'))
     except Exception as e:
         print("Error:", e)
         return Response(status=500)
-    
+
 @app.route('/upload-images')
 def upload():
     try:
-        if "userId" in session:
-                return render_template('uploadPage.html', username = session["username"], isAdmin = session["userIsAdmin"])
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            return render_template('uploadPage.html', username=user_info["username"], isAdmin=user_info["isAdmin"])
+        else:
+            return redirect(url_for('signin'))  # Redirect to signin if user not logged in
     except Exception as e:
         print("Error:", e)
         return Response(status=500)
-    
+
 @app.route('/Upload', methods=['POST'])
 def upload_images():
-    if 'files[]' not in request.files:
-        return "No files uploaded", 400
-    
-    files = request.files.getlist('files[]')
+    try:
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            if 'files[]' not in request.files:
+                return "No files uploaded", 400
 
-    TEMP_DIR = './temp/'
+            files = request.files.getlist('files[]')
 
-    if not os.path.exists(TEMP_DIR):
-        os.makedirs(TEMP_DIR)
+            TEMP_DIR = './temp/'
 
-    for file in files:
-        if file.filename != '':
-            filename = os.path.join(TEMP_DIR, file.filename)
-            file.save(filename)
-            save_image_to_mysql(session.get("userId"), filename)
-            
-    if os.path.exists(TEMP_DIR):
-        for file_name in os.listdir(TEMP_DIR):
-            file_path = os.path.join(TEMP_DIR, file_name)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        os.rmdir(TEMP_DIR)
+            if not os.path.exists(TEMP_DIR):
+                os.makedirs(TEMP_DIR)
 
-    return redirect(url_for('create'))
-    
+            for file in files:
+                if file.filename != '':
+                    filename = os.path.join(TEMP_DIR, file.filename)
+                    file.save(filename)
+                    save_image_to_mysql(user_info["userId"], filename)
+
+            if os.path.exists(TEMP_DIR):
+                for file_name in os.listdir(TEMP_DIR):
+                    file_path = os.path.join(TEMP_DIR, file_name)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                os.rmdir(TEMP_DIR)
+
+            return redirect(url_for('create'))
+    except Exception as e:
+        print("Error:", e)
+        return Response(status=500)
+
 @app.route('/create-video')
 def create():
     try:
-        if "userId" in session:
-            images = retrieve_image_from_mysql(session["userId"])
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            images = retrieve_image_from_mysql(user_info["userId"])
             imageData = []
             for i in images:
                 encoded_image = base64.b64encode(i.file_data).decode('utf-8')
-                img = {'data' : encoded_image, 'name': i.file_name}
+                img = {'data': encoded_image, 'name': i.file_name}
                 imageData.append(img)
-            
+
             audio = retrieve_audio_from_mysql(1)
             audioData = []
             for a in audio:
                 encoded_audio = base64.b64encode(a.file_data).decode('utf-8')
                 ad = {'data': encoded_audio, 'name': a.file_name}
-                audioData.append(ad)    
-            audio = retrieve_audio_from_mysql(session['userId'])
+                audioData.append(ad)
+            audio = retrieve_audio_from_mysql(user_info["userId"])
             for a in audio:
                 encoded_audio = base64.b64encode(a.file_data).decode('utf-8')
                 ad = {'data': encoded_audio, 'name': a.file_name}
-                audioData.append(ad)  
-               
-            return render_template('workspace.html', images = imageData, audio = audioData, username = session["username"], isAdmin = session["userIsAdmin"])
-        
+                audioData.append(ad)
+
+            return render_template('workspace.html', images=imageData, audio=audioData, username=user_info["username"], isAdmin=user_info["isAdmin"])
+
     except Exception as e:
         print("Error:", e)
         return Response(status=500)
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
-    if 'audioFile' not in request.files:
-        return "No audio file uploaded", 400
-    
-    audio_file = request.files['audioFile']
-    TEMP_DIR = './temp/'
+    try:
+        if "jwt_token" in session:
+            user_info = session["jwt_token"]
+            if 'audioFile' not in request.files:
+                return "No audio file uploaded", 400
 
-    if not os.path.exists(TEMP_DIR):
-        os.makedirs(TEMP_DIR)
+            audio_file = request.files['audioFile']
+            TEMP_DIR = './temp/'
 
-    if audio_file.filename != '':
-        filename = os.path.join(TEMP_DIR, audio_file.filename)
-        audio_file.save(filename)
-        save_audio_to_mysql(session.get("userId"), filename)
-            
-    if os.path.exists(TEMP_DIR):
-        for file_name in os.listdir(TEMP_DIR):
-            file_path = os.path.join(TEMP_DIR, file_name)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        os.rmdir(TEMP_DIR)
-    
-    return "Audio file uploaded successfully"
+            if not os.path.exists(TEMP_DIR):
+                os.makedirs(TEMP_DIR)
+
+            if audio_file.filename != '':
+                filename = os.path.join(TEMP_DIR, audio_file.filename)
+                audio_file.save(filename)
+                save_audio_to_mysql(user_info["userId"], filename)
+
+            if os.path.exists(TEMP_DIR):
+                for file_name in os.listdir(TEMP_DIR):
+                    file_path = os.path.join(TEMP_DIR, file_name)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                os.rmdir(TEMP_DIR)
+
+            return "Audio file uploaded successfully"
+    except Exception as e:
+        print("Error:", e)
+        return Response(status=500)
 
 if __name__ == '__main__':
     app.run(debug=True)
