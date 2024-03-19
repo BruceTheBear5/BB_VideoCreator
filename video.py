123da1f35932e30faaf42ddea9141671d91e0336
Saiyam20 <saiyam.jain@students.iiit.ac.in>
2024-03-19 16:50:35 +0530

    TransitionAndAudio

--- video.py
+++ video.py
@@ -1,13 +1,19 @@
 from moviepy.editor import ImageSequenceClip, AudioFileClip, concatenate_audioclips, concatenate_videoclips, CompositeVideoClip, ColorClip
-from moviepy.video.fx import scroll
+from moviepy.video.fx.all import scroll
 from PIL import Image
 import os
+import math
 
 video_name = 'output_video.mp4'
 width, height = 640, 480 
 supported_formats = [".jpeg", ".jpg", ".png", ".webp"]
+scroll_params = {
+    'x_speed': 0, 
+    'y_speed': 50,
+    'apply_to': 'mask'
+}
 
-def createVideo(imgFolderName, musicFileName, timePerImage, fadeIn = False, fadeOut = False, fadeInOut = False, scroll = False):
+def createVideo(imgFolderName, musicFileName, timePerImage, fadeIn = False, crossFadeIn=False, fadeOut = False, crossFadeOut=False, fadeInOut = False, crossFadeInOut = False, scrollEff=False):
     images = [img for img in os.listdir(imgFolderName) if any(img.endswith(format) for format in supported_formats)]
     resized_image_paths = []
     clips = []
@@ -22,12 +28,25 @@ def createVideo(imgFolderName, musicFileName, timePerImage, fadeIn = False, fade
             resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
             img.save(resized_img_path)
             resized_image_paths.append(resized_img_path)
-            clip = ImageSequenceClip([resized_img_path], fps=timePerImage)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
             clip = clip.set_duration(timePerImage)
             clip = clip.fadein(1).fadeout(0)
             clips.append(clip)
-
-        final_clip = black_screen
+        
+    elif(crossFadeIn):
+        for image in images:
+            img_path = os.path.join(imgFolderName, image)
+            img = Image.open(img_path)
+            img = img.convert('RGB')
+            img = img.resize((width, height))
+            resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
+            img.save(resized_img_path)
+            resized_image_paths.append(resized_img_path)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
+            clip = clip.set_duration(timePerImage)
+            clip = clip.crossfadein(1).crossfadeout(0)
+            clips.append(clip)
+        
     elif(fadeOut):
         for image in images:
             img_path = os.path.join(imgFolderName, image)
@@ -37,12 +56,25 @@ def createVideo(imgFolderName, musicFileName, timePerImage, fadeIn = False, fade
             resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
             img.save(resized_img_path)
             resized_image_paths.append(resized_img_path)
-            clip = ImageSequenceClip([resized_img_path], fps=timePerImage)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
             clip = clip.set_duration(timePerImage)
             clip = clip.fadein(0).fadeout(1)
             clips.append(clip)
-
-        final_clip = black_screen
+            
+    elif(crossFadeOut):
+        for image in images:
+            img_path = os.path.join(imgFolderName, image)
+            img = Image.open(img_path)
+            img = img.convert('RGB')
+            img = img.resize((width, height))
+            resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
+            img.save(resized_img_path)
+            resized_image_paths.append(resized_img_path)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
+            clip = clip.set_duration(timePerImage)
+            clip = clip.crossfadein(0).crossfadeout(1)
+            clips.append(clip)
+        
     elif(fadeInOut):
         for image in images:
             img_path = os.path.join(imgFolderName, image)
@@ -52,31 +84,40 @@ def createVideo(imgFolderName, musicFileName, timePerImage, fadeIn = False, fade
             resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
             img.save(resized_img_path)
             resized_image_paths.append(resized_img_path)
-            clip = ImageSequenceClip([resized_img_path], fps=timePerImage)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
             clip = clip.set_duration(timePerImage)
             clip = clip.fadein(0.5).fadeout(0.5)
             clips.append(clip)
-
-        final_clip = black_screen
-    elif (scroll):
-        for i in range(len(images)):
-            img_path = os.path.join(imgFolderName, images[i])
+            
+        
+    elif(crossFadeInOut):
+        for image in images:
+            img_path = os.path.join(imgFolderName, image)
             img = Image.open(img_path)
             img = img.convert('RGB')
             img = img.resize((width, height))
-            resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(images[i])[0] + ".jpg") 
+            resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
             img.save(resized_img_path)
             resized_image_paths.append(resized_img_path)
-            clip = ImageSequenceClip([resized_img_path], fps=timePerImage)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
             clip = clip.set_duration(timePerImage)
+            clip = clip.crossfadein(0.5).crossfadeout(0.5)
             clips.append(clip)
             
-        transition_duration = 1  
-        final_clips = []
-        for i in range(len(clips) - 1):
-            clip_with_transition = clips[i].fx(scroll, duration=transition_duration, y_speed=50)  # Adjust y_speed as needed
-            final_clips.append(clip_with_transition)
-            final_clips.append(clips[i + 1])
+    elif(scrollEff):
+        for image in images:
+            img_path = os.path.join(imgFolderName, image)
+            img = Image.open(img_path)
+            img = img.convert('RGB')
+            img = img.resize((width, height))
+            resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
+            img.save(resized_img_path)
+            resized_image_paths.append(resized_img_path)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
+            clip = clip.set_duration(timePerImage)
+            clip_scroll = clip.fx(scroll, **scroll_params)
+            clips.append(clip_scroll)
+            
     else:
         for image in images:
             img_path = os.path.join(imgFolderName, image)
@@ -86,29 +127,39 @@ def createVideo(imgFolderName, musicFileName, timePerImage, fadeIn = False, fade
             resized_img_path = os.path.join(imgFolderName, "resized_" + os.path.splitext(image)[0] + ".jpg") 
             img.save(resized_img_path)
             resized_image_paths.append(resized_img_path)
-            clip = ImageSequenceClip([resized_img_path], fps=timePerImage)
+            clip = ImageSequenceClip([resized_img_path], fps = 1)
             clip = clip.set_duration(timePerImage)
             clips.append(clip)
+        
 
-        final_clip = black_screen
+    final_clip = black_screen
         
     for clip in clips:
         final_clip = concatenate_videoclips([final_clip, clip])
 
     audio_clip = AudioFileClip(musicFileName)
     audio_duration = audio_clip.duration
+    # print("Original audio duration:", audio_duration)
+
     video_duration = final_clip.duration
-    repetitions = min(int(video_duration / audio_duration) + 1, 1)
+    # print("Video duration:", video_duration)
+
+    repetitions = max(math.ceil(float(video_duration) / float(audio_duration)) + 1, 1)
+    # print("Repetitions needed:", repetitions)
+
     audio_clips = [audio_clip] * repetitions
     concatenated_audio_clip = concatenate_audioclips(audio_clips)
     concatenated_audio_clip = concatenated_audio_clip.set_duration(video_duration)
+    # print("Concatenated audio duration:", concatenated_audio_clip.duration)
+
     final_clip = final_clip.set_audio(concatenated_audio_clip)
+
     
-    final_clip.write_videofile(video_name, fps=timePerImage)
+    final_clip.write_videofile(video_name, fps = 1)
 
     for resized_img_path in resized_image_paths:
         os.remove(resized_img_path)
 
     print("Video created successfully!")
 
-createVideo("./static/Images", "./try.mp3", 3, scroll== True)
+createVideo("./static/Images", "./try.mp3", 10, scrollEff = True)
